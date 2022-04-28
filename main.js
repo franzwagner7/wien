66a6a3f88cda737a6e0d4474347f66c5241fa144
Franz Wagner <franz.wagner@student.uibk.ac.at>
2022-04-28 17:32:12 +0200

    Layercontrol hinzugefügt

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 /* OGD Wien Beispiel */
+
+let stephansdom = {
+    lat: 48.20845089806038, 
+    lng: 16.373484275555537,
+    title: "Stephansdom"
+};
+
+let startLayer = L.tileLayer.provider("BasemapAT.grau")
+
+let map = L.map("map", {
+    center: [stephansdom.lat, stephansdom.lng],
+    zoom: 12,
+    layers: [
+        startLayer
+    ]
+})
+
+let layerControl = L.control.layers({
+    "BasemapAT Grau": startLayer
+}).addTo(map);
