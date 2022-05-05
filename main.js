04bcc88ddf27c64257a3b0c76baf254d688d5647
Franz Wagner <franz.wagner@student.uibk.ac.at>
2022-05-05 16:35:27 +0200

    Fußgängerzonen hinzugefügt

--- main.js
+++ main.js
@@ -90,3 +90,18 @@ async function loadLines(url) {
     L.geoJSON(geojson).addTo(overlay)
 }
 loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
+// Load Layer Fußgängerzonen WIen from Wien OGD as geoJSON
+async function loadZones(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson)
+
+    // Add to overlay
+    let overlay = L.featureGroup();
+    layerControl.addOverlay(overlay, "Fußgängerzonen");
+    overlay.addTo(map);
+
+    L.geoJSON(geojson).addTo(overlay)
+}
+loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
