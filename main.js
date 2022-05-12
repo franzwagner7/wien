45177d343324d2a213df69c672c77d5b4c93313e
Franz Wagner <franz.wagner@student.uibk.ac.at>
2022-05-12 18:31:56 +0200

    geojson sortiert

--- main.js
+++ main.js
@@ -189,6 +189,13 @@ loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&vers
 async function loadHotels(url) {
     let response = await fetch(url);
     let geojson = await response.json();
+    console.log(geojson.features);
+
+    // Hotels nach Name sortieren
+    geojson.features.sort(function(a, b){
+        return a.properties.BETRIEB.toLowerCase() > b.properties.BETRIEB.toLowerCase()
+    })
+
     let overlay = L.markerClusterGroup({
         disableClusteringAtZoom: 17
     });
